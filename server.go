7455c31c0f2f272e05864b05d6a5a8e54dff2bf9
Vitaly Haritonsky <vharitonsky@gmail.com>
2014-07-19 18:08:42 +0300

    init rewritter in go routines

--- server.go
+++ server.go
@@ -2,32 +2,44 @@ package main
 
 import (
 	"github.com/vharitonsky/goutil"
-	//"github.com/vharitonsky/model_matcher/lib"
-	"strings"
+	"log"
     "fmt"
+	"strings"
+	"sync"
 )
 
 var (
 	modelsMap = make(map[string][]Model)
 )
 
-type Model struct{
-    id, name string
+type Model struct {
+	id, name string
 }
 
-//func match(name string, callback_url string) {
-
-//}
+func match(name string, callback_url string) string {
+	return "ok"
+}
 
 func init() {
-	for line := range(goutil.ReadLines("data/cats.txt")) {
+	log.Print("Initializing models")
+	var wg sync.WaitGroup
+	models_count, categories_count := 0, 0
+	for line := range goutil.ReadLines("data/cats.txt") {
 		modelsMap[line] = make([]Model, 0)
-		for model_line := range(goutil.ReadLines("data/models/m_" + line + ".txt")) {
-			parts := strings.Split(model_line, "|")
-            m := Model{id: parts[0], name: parts[1]}
-			modelsMap[line] = append(modelsMap[line], m)
-		}
+		categories_count += 1
+    	wg.Add(1)
+		go func() {
+			for model_line := range goutil.ReadLines("data/models/m_" + line + ".txt") {
+				parts := strings.Split(model_line, "|")
+				m := Model{id: parts[0], name: parts[1]}
+				modelsMap[line] = append(modelsMap[line], m)
+				models_count += 1
+			}
+			wg.Done()
+		}()
 	}
+	wg.Wait()
+	log.Print(fmt.Sprintf("Matcher initialized with %d models from %d categories", models_count, categories_count))
 }
 
 func main() {
